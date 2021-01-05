4acb30ac137499a7c703cefbba80fd8bfc9a5544
manishmankar <manishmankar07m@gmail.com>
2021-01-05 21:16:16 +0530

    videoSlier done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css'
 //import {Route , Switch} from 'react-router-dom';
 import Navbar from './Component/Navbar/Navbar';
+import VideoSlider from "./Component/VideoSlider/VideoSlider"
+import IntroPage from "./Component/IntroPage/IntroPage"
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <VideoSlider/>
+     <IntroPage/>
      
     {/*} <Switch>
             <Route exact path='' component={} />
---
+++ src/Component/IntroPage/IntroPage.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./IntroPage.css";
+
+export default class IntroPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <div className="leftSide">
+            <h3>WE ARE</h3>
+            <h1>WEB-EXPERT</h1>
+            <p>
+              We are a full house digital branding & Development company based
+              in India providing Branding Services, Social Media Marketing,
+              Mobile App, Web Design & Web Development. Being one of the best
+              Digital company in NY We assure time bound services to our
+              customers and support them in developing winning strategies for
+              their businesses through innovative technologies. We at Stemslog
+              believe in the passion about bringing more with a merciless search
+              of triumph, we begin where other agencies stop.
+            </p>
+          </div>
+          <div className="rightSide"></div>
+        </div>
+      </div>
+    );
+  }
+}
