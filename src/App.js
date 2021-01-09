f1b1d9a24d1a44495738ccbabc9f9123f8db14fe
manishmankar <manishmankar07m@gmail.com>
2021-01-09 12:58:50 +0530

    detailspage and call to action page created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './App.css'
+import { CallToAction } from "@material-ui/icons";
+import React from "react";
+import "./App.css";
+import CalltoAction from "./Component/CtA/CalltoAction";
+import DetailsPage from "./Component/DetailsPage/DetailsPage";
+import ImageCard from "./Component/ImageCard/ImageCard";
 //import {Route , Switch} from 'react-router-dom';
-import Navbar from './Component/Navbar/Navbar';
-import VideoSlider from "./Component/VideoSlider/VideoSlider"
-import IntroPage from "./Component/IntroPage/IntroPage"
+import Navbar from "./Component/Navbar/Navbar";
+import VideoSlider from "./Component/VideoSlider/VideoSlider";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <VideoSlider/>
-     <IntroPage/>
-     
-    {/*} <Switch>
+      <Navbar />
+      <VideoSlider />
+      <DetailsPage />
+      <ImageCard />
+
+      <CalltoAction />
+
+      {/*} <Switch>
             <Route exact path='' component={} />
             <Route exact path='' component={} />
             
